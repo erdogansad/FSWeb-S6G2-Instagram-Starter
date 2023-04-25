bd73017b775383835d4090a296df9d239404fb81
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-04-25 17:29:26 +0300

    arama çubuğu componenti düzenlendi

--- src/bileşenler/AramaÇubuğu/AramaÇubuğu.js
+++ src/bileşenler/AramaÇubuğu/AramaÇubuğu.js
@@ -6,6 +6,7 @@ import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icon
 import "./AramaÇubuğu.css";
 
 const AramaÇubuğu = (props) => {
+  const {searchVal, setSearchVal} = props;
   return (
     <div className="search-bar-wrapper">
       <div className="social">
@@ -15,6 +16,8 @@ const AramaÇubuğu = (props) => {
         <input
           type="text"
           placeholder="Arama"
+          value={searchVal}
+          onChange={evt => setSearchVal(evt.target.value)}
         />
       </form>
       <div className="social-wrapper">
